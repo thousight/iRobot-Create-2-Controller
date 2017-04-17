bec0e854a415b53c20f81c0f38817fb761621db2
Mark Wen <markwenguojie94@gmail.com>
2017-04-17 00:42:18 -0400

    Initialize project

---
+++ app/src/main/java/www/markwen/space/irobot_create_2_controller/MainActivity.java
@@ -0,0 +1,13 @@
+package www.markwen.space.irobot_create_2_controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/www/markwen/space/irobot_create_2_controller/RobotActivity.java
@@ -0,0 +1,18 @@
+package www.markwen.space.irobot_create_2_controller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by markw on 4/17/2017.
+ */
+
+public class RobotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_robot);
+    }
+
+}

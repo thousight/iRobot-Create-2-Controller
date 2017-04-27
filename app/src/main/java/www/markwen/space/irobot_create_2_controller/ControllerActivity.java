eb76db2c811114a8499595e178998d1f1aad1087
Mark Wen <markwenguojie94@gmail.com>
2017-04-27 02:44:33 -0400

    More clean up and UI details settings

--- app/src/main/java/www/markwen/space/irobot_create_2_controller/ControllerActivity.java
+++ app/src/main/java/www/markwen/space/irobot_create_2_controller/ControllerActivity.java
@@ -2,12 +2,15 @@ package www.markwen.space.irobot_create_2_controller;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -29,7 +32,7 @@ public class ControllerActivity extends AppCompatActivity {
     private boolean isConnected = false;
     private MaterialDialog connectionDialog, connectingDialog;
     private VideoView videoView;
-    private ImageButton recordButton, leftButton, rightButton, upButton, downButton;
+    private ImageButton recordButton, stopRecordButton, leftButton, rightButton, upButton, downButton;
     private String robotIP = "";
     private static ControllerHttpClient httpClient;
 
@@ -40,10 +43,22 @@ public class ControllerActivity extends AppCompatActivity {
 
         videoView = (VideoView)findViewById(R.id.videoView);
         recordButton = (ImageButton)findViewById(R.id.recordButton);
+        stopRecordButton = (ImageButton)findViewById(R.id.stopRecordButton);
         leftButton = (ImageButton)findViewById(R.id.leftButton);
         upButton = (ImageButton)findViewById(R.id.upButton);
         rightButton = (ImageButton)findViewById(R.id.rightButton);
         downButton = (ImageButton)findViewById(R.id.downButton);
+        stopRecordButton.setVisibility(View.GONE);
+
+        // Get screen resolution
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
+
+        // Set VideoView height based on screen resolution
+        ViewGroup.LayoutParams params = videoView.getLayoutParams();
+        params.height = width * 9 / 16;
+        videoView.setLayoutParams(params);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE) !=  PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) !=  PackageManager.PERMISSION_GRANTED
@@ -62,7 +77,7 @@ public class ControllerActivity extends AppCompatActivity {
         connectionDialog = new MaterialDialog.Builder(this)
                 .title("Connecting")
                 .customView(R.layout.client_connect_dialog, false)
-                .cancelable(false)
+//                .cancelable(false)
                 .buttonsGravity(GravityEnum.END)
                 .neutralText("Connect")
                 .negativeText("Cancel")
@@ -73,8 +88,23 @@ public class ControllerActivity extends AppCompatActivity {
                         isConnected = true;
                         EditText robotIpEditText = (EditText) dialog.getView().findViewById(R.id.client_IP_input);
                         robotIP = robotIpEditText.getText().toString();
+
                         // Connect to robot
-                        sendCommandToServer("Connect");
+                        httpClient.testConnection(robotIP, new AsyncHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                setConnected();
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                Toast.makeText(ControllerActivity.this, "Connection failed: " + error.toString(), Toast.LENGTH_LONG).show();
+                                isConnected = false;
+                                connectionDialog.show();
+                            }
+                        });
+
+                        // Set up and show connecting dialog
                         connectingDialog = new MaterialDialog.Builder(ControllerActivity.this)
                                 .title("Connecting")
                                 .content("Connecting to " + robotIP)
@@ -101,7 +131,19 @@ public class ControllerActivity extends AppCompatActivity {
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                recordButton.setVisibility(View.GONE);
+                stopRecordButton.setVisibility(View.VISIBLE);
                 // TODO: start recording streamed video
+
+            }
+        });
+
+        stopRecordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recordButton.setVisibility(View.VISIBLE);
+                stopRecordButton.setVisibility(View.GONE);
+                // TODO: stop recording streamed video and save it to device
 
             }
         });
@@ -109,28 +151,28 @@ public class ControllerActivity extends AppCompatActivity {
         leftButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendCommandToServer("Left");
+                moveRobot("Left", 10);
             }
         });
 
         rightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendCommandToServer("Right");
+                moveRobot("Right", 10);
             }
         });
 
         upButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendCommandToServer("Up");
+                moveRobot("Up", 10);
             }
         });
 
         downButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendCommandToServer("Down");
+                moveRobot("Down", 10);
             }
         });
     }
@@ -147,26 +189,12 @@ public class ControllerActivity extends AppCompatActivity {
     "Connect" to initially connect to robot
     "Up" / "Down" / "Left" / "Right" to send robot directions command
      */
-    private void sendCommandToServer(String command) {
-        if (command.equals("Connect")) {
-            httpClient.testConnection(robotIP, new AsyncHttpResponseHandler() {
+    private void moveRobot(String direction, int speed) {
+        if (!robotIP.equals("")) {
+            httpClient.moveRobot(robotIP, direction, String.valueOf(speed), new AsyncHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                    setConnected();
-                }
-
-                @Override
-                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                    Toast.makeText(ControllerActivity.this, "Connection failed: " + error.toString(), Toast.LENGTH_LONG).show();
-                    isConnected = false;
-                    connectionDialog.show();
-                }
-            });
-        } else {
-            httpClient.moveRobot(robotIP, command, "10", new AsyncHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
+                    // Purposely left empty
                 }
 
                 @Override
